d02e2625811aebb0c581274bba339289949cc4ae
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2019-04-19 10:51:42 -0700

    First Commit

---
+++ XamarinFormsSingletonRepro/XamarinFormsSingletonRepro.Android/SingletonTabbedPageRenderer.cs
@@ -0,0 +1,19 @@
+﻿using Xamarin.Forms;
+
+using Android.Content;
+
+using XamarinFormsSingletonRepro;
+using XamarinFormsSingletonRepro.Droid;
+
+using Plugin.Badge.Droid;
+
+[assembly: ExportRenderer(typeof(SingletonTabbedPage), typeof(SingletonTabbedPageRenderer))]
+namespace XamarinFormsSingletonRepro.Droid
+{
+    public class SingletonTabbedPageRenderer : BadgedTabbedPageRenderer
+    {
+        public SingletonTabbedPageRenderer(Context context) : base(context)
+        {
+        }
+    }
+}
---
+++ XamarinFormsSingletonRepro/XamarinFormsSingletonRepro/App.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XamarinFormsSingletonRepro
+{
+    public class App : Application
+    {
+        public App() => MainPage = SingletonTabbedPage.Instance;
+    }
+
+    public class SingletonTabbedPage : TabbedPage
+    {
+        readonly static Lazy<SingletonTabbedPage> _instanceHolder = new Lazy<SingletonTabbedPage>(() => new SingletonTabbedPage());
+
+        public static SingletonTabbedPage Instance => _instanceHolder.Value;
+    }
+}
